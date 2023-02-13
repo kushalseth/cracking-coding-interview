a5c30714a1890de1c3ccebb7f908aea381c4cf07
Kushal Seth <kushal.seth@nagarro.com>
2023-02-14 03:03:08 +0530

    cracking coding Interview - chapter 01 - question 01 - IsUnique

---
+++ CrackingCodingInterview/CrackingCodingInterview/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CrackingCodingInterview.chapter01;
+
+Console.WriteLine("Hello, World!");
+
+// Chapter 01 -> Question 1: 
+_1_1_IsUnique _1_1_IsUnique = new _1_1_IsUnique();
+Console.WriteLine(_1_1_IsUnique.IsUnique("kushal seth"));
---
+++ CrackingCodingInterview/CrackingCodingInterview/chapter01/1_1_IsUnique.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrackingCodingInterview.chapter01
+{
+    internal class _1_1_IsUnique
+    {
+        public bool IsUnique(string input)
+        {
+            if (input.Length > 128) return false;
+
+            bool[] charset = new bool[128];
+
+            for(int i = 0; i < input.Length; i++)
+            {
+                char a = input[i];
+                if(charset[a])
+                {
+                    return false;
+                }
+                charset[a] = true;
+            }           
+            return false;
+        }
+    }
+}
